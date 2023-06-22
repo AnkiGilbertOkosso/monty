b0903d1701742be42f814986a569b4e4860887e2
Khaybee <kachieosuji11@gmail.com>
2023-06-22 12:39:49 -0700

    Updated

--- execute_instruction.c
+++ execute_instruction.c
@@ -14,29 +14,26 @@ int exec(char *element, stack_t **stack, unsigned int line_count, FILE *file)
 	instruction_t ints[] = {
 		{"push", __push},
 		{"pall", __pall},
-		{"pint", __pint},
-		{"pop", __pop},
+		{"pint", __pint}, {"pop", __pop},
 		{"swap", __swap},
-		{"add", __add},
-		{"nop", __nop},
-		{"div", __sub},
-		{"mul", __mul},
+		{"add", __add},	{"nop", __nop},
+		{"div", __sub}, {"mul", __mul},
 		{"mod", __mod},
 		{"pchar", __pchar},
 		{"pstr", __pstr},
-		{"rot1", __rot1},
 		{"rotr", __rotr},
+		{"rotl", __rotl},
 		{"stack", __stack},
 		{"queue", __queue},
 		{NULL, NULL}
-	}
+	};
 	unsigned int i = 0;
 	char *op;
 
 	op = strtok(element, " \n\t");
 	if (op && op[0] == '#')
 		return (0);
-	interpreter.line = strtok(NULL, " \n\t")
+	interpreter.line = strtok(NULL, " \n\t");
 		while (ints[i].opcode && op)
 		{
 			if (strcmp(op, ints[i].opcode) == 0)
@@ -44,8 +41,7 @@ int exec(char *element, stack_t **stack, unsigned int line_count, FILE *file)
 				ints[i].f(stack, line_count);
 				return (0);
 			}
-			i++;
-		}
+			i++;		}
 	if (op && ints[i].opcode == NULL)
 	{
 		fprintf(stderr, "L%d: unknown instruction %s\n", line_count, op);
@@ -62,7 +58,7 @@ int exec(char *element, stack_t **stack, unsigned int line_count, FILE *file)
  * @head: head of a stack
  */
 
-voide free_stadck(stack_t *head)
+void free_stack(stack_t *head)
 {
 	stack_t *stack;
 
