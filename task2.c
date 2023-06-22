1dd85009accc081b056a4f9c220ebdef57de9a97
Khaybee <kachieosuji11@gmail.com>
2023-06-22 15:41:36 -0700

    task 2

---
+++ task2.c
@@ -0,0 +1,25 @@
+#include "monty.c"
+
+/**
+ * __pop - prints a top
+ * @head: stack head
+ * @line_number: line_number
+ * Return: Nothing
+ */
+
+void __pop(stack_t **head, unsigned int line_number)
+{
+	stack_t *stack;
+
+	if (*head == NULL)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+		fclose(interpreter, file);
+		free(interpreter, elements);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	stack = *head;
+	*head =  stack->next;
+	free(stack);
+}
